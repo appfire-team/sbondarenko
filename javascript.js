c34cd2bb752ca5b95b07479ce850e55762664353
serhiibondarenko-collab <serhii.bondarenko@appfire.com>
2026-04-21 12:05:47 +0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,5 @@
+        out.println(" {csv:id=1|enableSorting=false|enableHighlighting=false}");
+        out.println("h1, h2, h3");
+        out.println("d1, d2, d3");
+        out.println("{csv}");
+     
